cfb77260c85f8970f65ae00629e89c4faa64c810
Ryan1431 <ryan1431@gmail.com>
2022-10-30 23:41:03 -0400

    Make board sizing dynamic

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { fields } from '../utility/data';
 import './Board.css';
 import { Field } from './Field';
 
@@ -9,18 +8,19 @@ interface BoardProps {
 }
 
 export const Board = ({ pieces, setPieces}: BoardProps) => {
-  
 
-
+  const [size] = useState<number>(10);
+  const [fields] = useState<undefined[]>(new Array(size*size).fill(''));
 
   return (
-    <div id="board">
+    <div 
+      id="board" 
+      style={{gridTemplateColumns: `repeat(${size}, 1fr)`, 
+              gridTemplateRows: `repeat(${size}, 1fr)`}}>
       {fields.map((f, i) => {
-        let coords = {x: (i+1) % 10 || 10, y: Math.ceil((i + 1)/10)}
+        let coords = {x: (i+1) % size || size, y: Math.ceil((i + 1)/size)}
         return <Field pieces={pieces} setPieces={setPieces} coords={coords}/>
-      }
-        
-      )}
+      })}
     </div>
   )
 }
--- src/utility/data.ts
+++ src/utility/data.ts
@@ -1,11 +1,9 @@
 
 
-let fields = new Array(10*10).fill('field');
-fields = fields.map((f, i) => f + (i + 1));
+
 
 let characters = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
 export {
-  fields,
   characters,
 }

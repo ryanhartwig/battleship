c8dc630310db71079e34cc368fa718198f3fd247
Ryan1431 <ryan1431@gmail.com>
2022-11-05 11:17:29 -0400

    Setup store items and layout

---
+++ src/utility/storeIcons.ts
@@ -0,0 +1,11 @@
+import { MdDirectionsBoat } from 'react-icons/md';
+import { GiSubmarineMissile, GiMissileLauncher, GiUnlitBomb, GiNuclearBomb, GiFallingBomb } from 'react-icons/gi';
+
+export const itemIcons = {
+  segment: MdDirectionsBoat,
+  ranged: GiSubmarineMissile,
+  longranged: GiMissileLauncher,
+  bomb: GiUnlitBomb,
+  directional: GiFallingBomb,
+  atomic: GiNuclearBomb,
+};

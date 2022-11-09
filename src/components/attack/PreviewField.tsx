8d84c12667b2a85cc707569121c3734721f8a2b5
Ryan1431 <ryan1431@gmail.com>
2022-11-09 18:26:08 -0500

    Add visuals to out of bounds fields

--- src/components/attack/PreviewField.tsx
+++ src/components/attack/PreviewField.tsx
@@ -7,6 +7,8 @@ import { characters } from '../../utility/data';
 import { Coord } from './Preview';
 import './PreviewField.css';
 
+import { TiWaves } from 'react-icons/ti';
+
 interface PreviewFieldProps {
   coords: Coord;
   selected: string;
@@ -42,27 +44,42 @@ export const PreviewField = ({ coords, selected, max, action }: PreviewFieldProp
     [action.hits, action.x, action.y, coords.x, coords.y]
   );
 
-  return (
-    <div id={`preview_${x}-${y}`} className={clsx('preview-field', { selected: selected === `${x}-${y}`, right: max.x === x, bottom: max.y === y }, { oob: x < 1 || y < 1 || x > size + moveLevels - 1 || y > size + moveLevels - 1 })}>
-      {/* Coordinates */}
-      <div>
-        <p>
-          {characters[y] || 'Z'}
-          {x}
-        </p>
-      </div>
+  const isOob = useCallback(
+    (x: number, y: number) => {
+      return x < 1 || y < 1 || x > size + moveLevels - 1 || y > size + moveLevels - 1;
+    },
+    [moveLevels, size]
+  );
 
-      {/* Players Hit */}
-      {!!hits.length ? (
-        <div>
-          <p>
-            {hits.map((u) => (
-              <span className={clsx('preview-hit', { sunk: isSunk(u.id) })}>{u.initial} </span>
-            ))}
-          </p>
+  return (
+    <div id={`preview_${x}-${y}`} className={clsx('preview-field', { selected: selected === `${x}-${y}`, right: max.x === x, bottom: max.y === y }, { oob: isOob(x, y) })}>
+      {/* Coordinates */}
+      {isOob(x, y) ? (
+        <div style={{ color: 'rgba(255,255,255,0.8)', fontSize: '30px' }}>
+          <TiWaves />
         </div>
       ) : (
-        <div></div>
+        <>
+          <div>
+            <p>
+              {characters[y] || 'Z'}
+              {x}
+            </p>
+          </div>
+
+          {/* Players Hit */}
+          {!!hits.length ? (
+            <div>
+              <p>
+                {hits.map((u) => (
+                  <span className={clsx('preview-hit', { sunk: isSunk(u.id) })}>{u.initial} </span>
+                ))}
+              </p>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </>
       )}
     </div>
   );
